0553551ced2f621bbabe30d5c9b7732a1935de89
Stephen Martin <mediabytes@icloud.com>
2018-08-16 01:19:23 -0400

    added password rules ui

--- src/views/Administration/PasswordRules.jsx
+++ src/views/Administration/PasswordRules.jsx
@@ -9,6 +9,7 @@ import Page from 'views/Page/Page';
 import GridItem from "components/Grid/GridItem.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
 import CustomInput from "components/CustomInput/CustomInput.jsx";
+import CustomButton from "components/CustomButtons/Button.jsx";
 
 const styles = theme => ({
 	content: {
@@ -32,17 +33,24 @@ const styles = theme => ({
 			margin: 'auto',
 			marginLeft: theme.spacing.unit * 7,
 			marginRight: theme.spacing.unit * 7,
-			marginTop: theme.spacing.unit * 1
+			marginTop: theme.spacing.unit * 1,
+			textAlign: 'center',
 		},
 		[theme.breakpoints.down('xs')]: {
 			display: 'block',
 			margin: 'auto',
 			marginTop: theme.spacing.unit * 1,
+			textAlign: 'center'
+
 		},
 	},
 	grid: {
 		width: "100%",
-		height: "60vh"
+	},
+	paper: {
+		paddingTop: 70,
+		paddingBottom: 30,
+
 	}
 })
 
@@ -52,23 +60,63 @@ class PasswordRules extends Component {
 		const { classes } = this.props;
 		return (
 			<div>
-				<Page><CssBaseline />
+				<Page titleCaption={ 'Password Rules' }><CssBaseline />
 					<GridContainer className={classes.content}>
-						<Paper>
+						<Paper className={classes.paper}>
 							<GridContainer>
 								<GridItem xs={12}>
 									<GridContainer className={classes.grid}>
-									<GridItem md={1}/>
-										<GridItem xs={8} md={5}>
-											<Typography variant={'body2'} gutterBottom> Password Expiration Period (Days)</Typography>
+										<GridItem md={1}/>
+										<GridItem xs={12} sm={12} md={8}>
+											<Typography style={{ paddingTop: 33 }} variant={'body2'}> Password Expiration Period (Days)</Typography>
 										</GridItem>
-										<GridItem xs={3}/>
-										<GridItem xs={2} sm={2}>
+										<GridItem xs={12} sm={12} md={2}>
 											<CustomInput
 												inputProps={{
 													placeholder: "45"
 												}}
 											/>
+										</GridItem>
+									</GridContainer>
+									<GridContainer className={classes.grid}>
+										<GridItem md={1}/>
+										<GridItem xs={12} sm={12} md={8}>
+											<Typography style={{ paddingTop: 33 }} variant={'body2'}>Minimum Character Length</Typography>
+										</GridItem>
+										<GridItem xs={12} sm={12} md={2}>
+											<CustomInput
+												inputProps={{
+													placeholder: "8"
+												}}
+											/>
+										</GridItem>
+									</GridContainer>
+									<GridContainer className={classes.grid}>
+										<GridItem md={1}/>
+										<GridItem xs={12} sm={12} md={8}>
+											<Typography style={{ paddingTop: 33 }} variant={'body2'}>Minimum Number of Special Characters</Typography>
+										</GridItem>
+										<GridItem xs={12} sm={12} md={2}>
+											<CustomInput
+												inputProps={{
+													placeholder: "1"
+												}}
+											/>
+										</GridItem>
+									</GridContainer>
+									<GridContainer className={classes.grid}>
+										<GridItem style={{marginTop: 140}} align={'right'} md={12}>
+											<CustomButton
+												color={'white'}
+												style={{marginRight: 20, color: 'black'}}
+											>
+												Cancel
+											</CustomButton>
+											<CustomButton
+												color={'info'}
+											>
+												Save
+											</CustomButton>
 										</GridItem>
 									</GridContainer>
 								</GridItem>
