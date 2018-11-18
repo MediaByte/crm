8f27dccdbc45f1a7ad879340c38adb530fe5779d
Joshuacarey <joshcarey1@gmail.com>
2018-11-17 19:09:32 -0600

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -22,7 +22,7 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 
 //material ui icons
 import SearchIcon from '@material-ui/icons/Search';
-import Calendar from '@material-ui/icons/Search';
+import Calendar from '@material-ui/icons/Calendar';
 import MenuIcon from '@material-ui/icons/Menu';
 import Laptop from '@material-ui/icons/Laptop';
 import People from '@material-ui/icons/People';
