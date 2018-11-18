4fc8e423c3b251e157ce95e0a1b28c1327f0f9a1
Joshuacarey <joshcarey1@gmail.com>
2018-11-18 10:55:05 -0600

    Update NavigationColumn.jsx

--- src/components/Navigation/NavigationColumn.jsx
+++ src/components/Navigation/NavigationColumn.jsx
@@ -27,7 +27,7 @@ import Laptop from '@material-ui/icons/Laptop';
 import People from '@material-ui/icons/People';
 import Event from '@material-ui/icons/Event';
 import CalendarTodayOutlined from '@material-ui/icons/CalendarTodayOutlined';
-import AccountBalance from '@material-ui/icons/AccountBalance';
+import AccountBalance from '@material-ui/icons/AccountBalanceOutlined';
 import CreditCard from '@material-ui/icons/CreditCard';
 import MailOutline from '@material-ui/icons/MailOutline';
 
