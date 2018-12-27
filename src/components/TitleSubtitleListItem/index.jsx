d0d26c32f8999512907a05462a1f50076897c087
danlugo92 <danlugo92@gmail.com>
2018-12-27 12:31:50 -0400

    <TitleSubtitleListItem />

---
+++ src/components/TitleSubtitleListItem/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+import Hidden from '@material-ui/core/Hidden'
+import IconButton from '@material-ui/core/IconButton'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import ListItem from '@material-ui/core/ListItem'
+
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
+
+/**
+ * @typedef {object} Props
+ * @prop {number} id
+ * @prop {((id: number) => void)=} onClick (Optional) Handler for the onClick
+ * event called with the id of the item.
+ * @prop {boolean|null=} selected Pass true to highlight the item
+ * compared to its siblings.
+ * @prop {string|null|boolean=} subtitle Text that will rendered below
+ * the main title in smaller type.
+ * @prop {string} title Main text that gets rendered for the item.
+ */
+
+export {} // stop jsdoc comments from merging
+
+/**
+ * A simple list item to show a a title and a subtitle beneath it. To be wrapped
+ * inside a material-ui's `<List />`.
+ * @augments React.PureComponent<Props>
+ */
+export default class TitleSubtitleListItem extends React.PureComponent {
+  onClick = () => {
+    const { onClick, id } = this.props
+
+    onClick(id)
+  }
+
+  render() {
+    const { selected, subtitle, title } = this.props
+
+    return (
+      <ListItem selected={selected} onClick={this.onClick}>
+        <ListItemText primary={title} secondary={subtitle} />
+        <Hidden smUp>
+          {/* TODO: Why smUp here?*/}
+          <ListItemSecondaryAction>
+            <IconButton>
+              <KeyboardArrowRight />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </Hidden>
+      </ListItem>
+    )
+  }
+}
