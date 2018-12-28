2a56d47c09f9f6381f5595fd23c27acad56646c3
danlugo92 <danlugo92@gmail.com>
2018-12-28 11:29:52 -0400

    Palette (styles) file

---
+++ src/res/Palette.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef {import('react').CSSProperties} CSSProperties
+ */
+
+/**
+ * Set a container to take 100% of its parent's width.
+ * @type {CSSProperties}
+ */
+export const width100Percent = {
+  width: '100%',
+}
