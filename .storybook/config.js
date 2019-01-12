97a1f5abf517d62f8b5798a03882d1d84b5af545
danlugo92 <danlugo92@gmail.com>
2019-01-12 18:30:27 -0400

    viewport addon for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/react'
+import { configureViewport } from '@storybook/addon-viewport'
 
 const req = require.context('../src/components', true, /\.stories\.jsx?$/)
 
