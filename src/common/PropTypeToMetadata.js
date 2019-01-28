80177473f1750349d46673ccdd650c2b0bf2ccf0
danlugo92 <danlugo92@gmail.com>
2019-01-28 19:06:52 -0400

    proptype metadata

---
+++ src/common/PropTypeToMetadata.js
@@ -0,0 +1,29 @@
+/**
+ * @type {Record<string, string>}
+ */
+export const typeToIconName = {
+  address: 'pindrop',
+  checkbox: 'checkbox',
+  date: 'calendar',
+  memo: 'longText',
+  phone: 'phone',
+  picklist: 'list',
+  radio: 'radiobutton',
+  textfield: 'text',
+  time: 'clock',
+}
+
+/**
+ * @type {Record<string, string>}
+ */
+export const typeToReadableName = {
+  address: 'Address',
+  checkbox: 'Checkbox',
+  date: 'Date',
+  memo: 'Memo',
+  phone: 'Phone',
+  picklist: 'Picklist',
+  radio: 'Radio',
+  textfield: 'Text Field',
+  time: 'Time',
+}
