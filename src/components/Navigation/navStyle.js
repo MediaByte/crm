a904a36aa0d07045f957027c2ae933285608eebf
Daniel Lugo <user.email>
2019-04-09 17:16:56 -0500

    remove unnecesary style

--- src/components/Navigation/navStyle.js
+++ src/components/Navigation/navStyle.js
@@ -62,9 +62,6 @@ const navStyles = theme => ({
     display: 'flex',
   },
   appBar: {
-    [theme.breakpoints.up('sm')]: {
-      zIndex: theme.zIndex.drawer + 1,
-    },
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
