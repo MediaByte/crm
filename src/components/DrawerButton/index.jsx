78e19a93d6b147fa660b7c06686ef1e9a24d72c7
Daniel Lugo <user.email>
2019-04-25 11:25:23 -0500

    typings, prop for location of sec text

--- src/components/DrawerButton/index.jsx
+++ src/components/DrawerButton/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import {
   Typography,
   withStyles,
@@ -6,8 +6,8 @@ import {
   ListItemText,
   ListItemSecondaryAction,
   Grid,
-  Divider,
 } from '@material-ui/core'
+
 import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 
 const styles = {
@@ -16,37 +16,50 @@ const styles = {
   },
 }
 
-const DrawerButton = ({
-  classes,
-  primaryText,
-  secondaryText,
-  secondaryTextToRight,
-  onClick,
-  divider,
-}) => (
-  <Fragment>
-    <ListItem className={classes.listItem} button onClick={onClick}>
-      <ListItemText
-        primary={primaryText}
-        secondary={!secondaryTextToRight && secondaryText}
-      />
-      <ListItemSecondaryAction>
-        {secondaryTextToRight ? (
-          <Grid container alignItems="center">
-            <Typography color="textSecondary">{secondaryText}</Typography>
-            <ChevronRightIcon color="disabled" fontSize="small" />
-          </Grid>
-        ) : (
-          <ChevronRightIcon color="disabled" fontSize="small" />
-        )}
-      </ListItemSecondaryAction>
-    </ListItem>
-    {divider && (
-      <li>
-        <Divider />
-      </li>
-    )}
-  </Fragment>
-)
+/**
+ * @typedef {object} Props
+ * @prop {Record<keyof typeof styles, string>} classes
+ * @prop {string} primaryText
+ * @prop {string=} secondaryText
+ * @prop {boolean=} secTextAtBottom
+ * @prop {(() => void)=} onClick
+ */
 
+/**
+ * @augments React.PureComponent<Props>
+ */
+class DrawerButton extends React.PureComponent {
+  render() {
+    const {
+      classes,
+      primaryText,
+      secondaryText,
+      secTextAtBottom,
+      onClick,
+    } = this.props
+
+    return (
+      <ListItem className={classes.listItem} button onClick={onClick}>
+        <ListItemText
+          primary={primaryText}
+          secondary={secTextAtBottom && secondaryText}
+        />
+        <ListItemSecondaryAction>
+          {!secTextAtBottom && secondaryText ? (
+            <Grid container alignItems="center">
+              <Typography color="textSecondary">{secondaryText}</Typography>
+              <ChevronRightIcon color="disabled" fontSize="small" />
+            </Grid>
+          ) : (
+            <ChevronRightIcon color="disabled" fontSize="small" />
+          )}
+        </ListItemSecondaryAction>
+      </ListItem>
+    )
+  }
+}
+
+/**
+ * Needs to be wrapped in `<List />` to work correctly.
+ */
 export default withStyles(styles)(DrawerButton)
