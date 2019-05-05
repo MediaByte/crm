fb0c7549994fca7451778ee0b31764040794fb8e
Daniel Lugo <user.email>
2019-05-05 11:32:22 -0500

    <NodeEditor />

---
+++ src/components/NodeEditor/index.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import Grid from '@material-ui/core/Grid'
+import List from '@material-ui/core/List'
+import Typography from '@material-ui/core/Typography'
+
+import DrawerButton from 'components/DrawerButton'
+
+/**
+ * @typedef {object} Props
+ * @prop {React.ComponentType<import('@material-ui/core/SvgIcon').SvgIconProps>=} icon
+ * @prop {string} label
+ * @prop {(() => void)=} onClickDeactivate
+ * @prop {(() => void)=} onClickIcon
+ * @prop {(() => void)=} onClickLabel
+ */
+
+/**
+ * @augments React.PureComponent<Props>
+ */
+export default class NodeEditor extends React.PureComponent {
+  render() {
+    const {
+      icon,
+      label,
+      onClickDeactivate,
+      onClickIcon,
+      onClickLabel,
+    } = this.props
+
+    console.log(`label: ${label}`)
+
+    return (
+      <Grid alignContent="center" direction="column" container>
+        <Typography>
+          You can only change a node's label but not the name. The label is a
+          free form bla bla bla
+        </Typography>
+
+        <List>
+          <DrawerButton
+            onClick={onClickLabel}
+            primaryText="Label"
+            secondaryText={label}
+          />
+        </List>
+
+        <Typography>ICON</Typography>
+
+        <List>
+          <DrawerButton icon={icon} onClick={onClickIcon} />
+        </List>
+
+        <Typography>
+          Deactivating a node bla bla bla bla bla bla bla...
+        </Typography>
+
+        <List>
+          <DrawerButton
+            onClick={onClickDeactivate}
+            primaryText="Deactivate"
+            primaryTextRed
+          />
+        </List>
+      </Grid>
+    )
+  }
+}
