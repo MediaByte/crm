1fea745b6cc26a44f937be1ad36734fdff7a3648
Daniel Lugo <user.email>
2019-05-07 14:50:21 -0500

    reactivate/deactivate

--- src/components/NodeEditor/index.jsx
+++ src/components/NodeEditor/index.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 
+import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import List from '@material-ui/core/List'
 import Typography from '@material-ui/core/Typography'
 
+import DeleteOutline from '@material-ui/icons/DeleteOutline'
+
 import DrawerButton from 'components/DrawerButton'
+
+const alignSelfCenter = { alignSelf: 'center' }
 
 /**
  * @typedef {object} Props
  * @prop {React.ComponentType<import('@material-ui/core/SvgIcon').SvgIconProps>=} icon
+ * @prop {boolean} isNodeActive If set to true, a deactivate button will be
+ * rendered, otherwise a reactivate button will be.
  * @prop {string} label
  * @prop {(() => void)=} onClickDeactivate
  * @prop {(() => void)=} onClickIcon
  * @prop {(() => void)=} onClickLabel
+ * @prop {(() => void)=} onClickReactivate
  */
 
 /**
@@ -22,18 +30,27 @@ export default class NodeEditor extends React.PureComponent {
   render() {
     const {
       icon,
+      isNodeActive,
       label,
       onClickDeactivate,
       onClickIcon,
       onClickLabel,
+      onClickReactivate,
     } = this.props
 
     return (
-      <Grid alignContent="center" direction="column" container>
-        <Typography>
-          You can only change a node's label but not the name. The label is a
-          free form bla bla bla
-        </Typography>
+      <Grid
+        alignContent="center"
+        container
+        direction="column"
+        justify="flex-start"
+      >
+        <Grid item>
+          <Typography>
+            You can only change a node's label but not the name. The label is a
+            free form bla bla bla
+          </Typography>
+        </Grid>
 
         <List>
           <DrawerButton
@@ -49,17 +66,23 @@ export default class NodeEditor extends React.PureComponent {
           <DrawerButton icon={icon} onClick={onClickIcon} />
         </List>
 
-        <Typography>
-          Deactivating a node bla bla bla bla bla bla bla...
-        </Typography>
+        <Grid item style={alignSelfCenter}>
+          <Typography>
+            {isNodeActive
+              ? 'Deactivating a node bla bla bla bla bla bla bla...'
+              : 'Reactivating a node bla bla bla bla'}
+          </Typography>
+        </Grid>
 
-        <List>
-          <DrawerButton
-            onClick={onClickDeactivate}
-            primaryText="Deactivate"
-            primaryTextRed
-          />
-        </List>
+        <Grid item style={alignSelfCenter}>
+          <Button
+            color={isNodeActive ? 'secondary' : 'primary'}
+            onClick={isNodeActive ? onClickDeactivate : onClickReactivate}
+          >
+            <DeleteOutline />
+            {isNodeActive ? 'Deactivate' : 'Reactivate'}
+          </Button>
+        </Grid>
       </Grid>
     )
   }
