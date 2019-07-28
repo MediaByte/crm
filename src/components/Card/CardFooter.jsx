0640bfc1d339b2a8a35a9d55475d64fa6abba368
Joshuacarey <joshcarey1@gmail.com>
2019-07-28 12:22:59 -0500

    Update CardFooter.jsx

--- src/components/Card/CardFooter.jsx
+++ src/components/Card/CardFooter.jsx
@@ -8,7 +8,7 @@ import withStyles from '@material-ui/core/styles/withStyles'
 // @material-ui/icons
 
 // core components
-import cardFooterStyle from './cardFooterStyle.jsx'
+import cardFooterStyle from './cardFooterStyle.js'
 
 function CardFooter({ ...props }) {
   const {
